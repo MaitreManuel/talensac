e9a4cbd54d728a5b7d2c24c52f44532b23208035
Trystan Eveno <trystan.eveno@lephare.com>
2017-10-29 22:15:07 +0100

    :rocket: deploy

--- src/AppBundle/Resources/components/history/Main.js
+++ src/AppBundle/Resources/components/history/Main.js
@@ -3,15 +3,17 @@ import Parallax from 'parallax-js';
 
 class Root extends React.Component {
     componentDidMount() {
-        var scene = document.getElementById('scene');
-        var parallaxInstance = new Parallax(scene);
+        var scene = document.getElementById('scene'),
+            parallaxInstance = new Parallax(scene);
+
+        parallaxInstance.limit(20, 0);
     }
     render() {
         return (
             <section id="History">
                 <div id="scene" className="scene">
-                    <div data-depth="0.2"><img src="/bundles/peoples.png" alt=""/></div>
-                    <div data-depth="0.6"><img src="/bundles/prehaut.png" alt=""/></div>
+                    <div data-depth="0.6"><img src="/bundles/marche.jpg" alt=""/></div>
+                    {/* <div data-depth="0.2"><img src="/bundles/prehaut.png" alt=""/></div> */}
                 </div>
             </section>
         );
