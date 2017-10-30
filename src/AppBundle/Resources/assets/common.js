3dc259fc969b98616a43066a6cd8617e7fc1793b
Trystan Eveno <trystan.eveno@lephare.com>
2017-10-30 23:09:51 +0100

    add assets

--- src/AppBundle/Resources/assets/common.js
+++
@@ -1,9 +0,0 @@
-import jQuery from 'jquery';
-import bootstrap from 'bootstrap';
-import ScrollMagic from 'scrollmagic';
-
-import './common.scss';
-
-jQuery(document).ready(() => {
-    console.log("COUCOU je marche");
-});
