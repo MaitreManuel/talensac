a87a0a2bbaf50105f74f1079c8f6536c47c7ccf7
Trystan Eveno <trystan.eveno@lephare.com>
2017-11-02 04:44:34 +0100

    :rocket: deploy

--- src/AppBundle/Resources/components/common/Footer.js
+++ src/AppBundle/Resources/components/common/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <footer id="Footer" className="container-fluid p-relative opacity-dark">
                 <div className="row">
                     <div className="col-12 text-center">
-                        <span><i className="fa fa-copyright" aria-hidden="true"></i> 2017 - Trystan Eveno</span>
+                        <span><strong>&copy;</strong> 2017 - Trystan Eveno</span>
                     </div>
                 </div>
             </footer>
